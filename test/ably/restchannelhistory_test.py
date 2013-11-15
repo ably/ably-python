c8978185ba2b530b810d1b018c13afdbdf2558e5
Jason Choy <jjwchoy@gmail.com>
2013-11-15 10:29:14 +0000

    Updated rest channel nhistory

--- test/ably/restchannelhistory_test.py
+++ test/ably/restchannelhistory_test.py
@@ -21,10 +21,47 @@ class TestRestChannelHistory(unittest.TestCase):
                 port=test_vars["port"],
                 tls_port=test_vars["tls_port"],
                 tls=test_vars["encrypted"])
+        cls.time_offset = cls.ably.time() - int(time.time())
 
     @property
     def ably(self):
         return TestRestChannelHistory.ably
 
     def test_publish_events_various_datatypes(self):
-        pass
+        history0 = TestRestChannelHistory.ably.channels.get('persisted:channelhistory_types')
+        history0.publish('history0', True)
+        history0.publish('history1', 24)
+        history0.publish('history2', 24.234)
+        history0.publish('history3', 'This is a string message')
+        history0.publish('history4', bytearray('This is a byte[] message', 'utf8'))
+        history0.publish('history5', {'test': 'This is a JSONObject message payload'})
+        history0.publish('history6', ['This is a JSONArray message payload'])
+
+        # Wait for the history to be persisted
+        time.sleep(16)
+
+        messages = history0.history()
+
+        self.assertIsNotNone(messages, msg="Expected non-None messages")
+        self.assertEquals(7, len(messages), msg="Expected 7 messages")
+        
+        message_contents = dict((m.name, m.data) for m in messages)
+
+        self.assertEquals(True, message_contents["publish0"],
+                msg="Expect publish0 to be Boolean(true)")
+        self.assertEquals(24, int(message_contents["publish1"]),
+                msg="Expect publish1 to be Int(24)")
+        self.assertEquals(24.234, float(message_contents["publish2"]),
+                msg="Expect publish2 to be Double(24.234)")
+        self.assertEquals("This is a string message payload",
+                message_contents["publish3"],
+                msg="Expect publish3 to be expected String)")
+        self.assertEquals("This is a byte[] message payload",
+                message_contents["publish4"],
+                msg="Expect publish4 to be expected byte[]")
+        self.assertEquals({"test": "This is a JSONObject message payload"},
+                json.loads(message_contents["publish5"]),
+                msg="Expect publish5 to be expected JSONObject")
+        self.assertEquals(["This is a JSONArray message payload"],
+                json.loads(message_contents["publish6"]),
+                msg="Expect publish6 to be expected JSONObject")
