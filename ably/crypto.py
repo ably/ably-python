08e77e62d576d83e28362c527ca2b149fb343f58
Jason Choy <jjwchoy@gmail.com>
2013-12-25 20:42:06 +0000

    Added random key and iv generation

--- ably/crypto.py
+++ ably/crypto.py
@@ -1,13 +1,14 @@
 import six
 from Crypto.Cipher import AES
+from Crypto import Random
 
 class CbcChannelCipher(object):
-    def __init__(self, algorithm="AES", secret_key=None, iv=None):
+    def __init__(self, secret_key=None, iv=None, algorithm='AES'):
+        self.__secret_key = secret_key or self.__random(32)
+        self.__iv = iv or self.__random(16)
+        self.__block_size = len(self.__iv)
         self.__algorithm = algorithm
-        self.__block_size = len(iv)
-        self.__secret_key = secret_key
-        self.__iv = iv
-        self.__encryptor = AES.new(self.__secret_key, AES.MODE_CBC, iv)
+        self.__encryptor = AES.new(self.__secret_key, AES.MODE_CBC, self.__iv)
 
     def __pad(self, data):
         padding_size = self.__block_size - (len(data) % self.__block_size)
@@ -19,15 +20,26 @@ class CbcChannelCipher(object):
         padding_size = data[-1] if six.PY3 else ord(data[-1])
         return data[:-padding_size]
 
+    def __random(self, length):
+        rndfile = Random.new()
+        return rndfile.read(length)
+
     def encrypt(self, plaintext):
         padded_plaintext = self.__pad(plaintext)
         encrypted = self.__iv + self.__encryptor.encrypt(padded_plaintext)
         self.__iv = encrypted[-self.__block_size:]
         return encrypted
-        
 
     def decrypt(self, ciphertext):
         iv = ciphertext[:self.__block_size]
         ciphertext = ciphertext[self.__block_size:]
         decryptor = AES.new(self.__secret_key, AES.MODE_CBC, iv)
         return self.__unpad(decryptor.decrypt(ciphertext))
+
+    @property
+    def secret_key(self):
+        return self.__secret_key
+
+    @property
+    def iv(self):
+        return self.__iv
