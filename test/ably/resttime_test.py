758832c483ec800eda7294426ff38602e6ba47a4
Jason Choy <jjwchoy@gmail.com>
2014-02-11 14:07:36 +0000

    Fixed resttime test

--- test/ably/resttime_test.py
+++ test/ably/resttime_test.py
@@ -3,8 +3,9 @@ from __future__ import absolute_import
 import time
 import unittest
 
-from ably.exceptions import AblyException
-from ably.rest import AblyRest
+from ably import AblyException
+from ably import AblyRest
+from ably import Options
 
 from test.ably.restsetup import RestSetup
 
@@ -13,11 +14,11 @@ test_vars = RestSetup.get_test_vars()
 
 class TestRestTime(unittest.TestCase):
     def test_time_accuracy(self):
-        ably = AblyRest(key=test_vars["keys"][0]["key_str"],
+        ably = AblyRest(Options.with_key(test_vars["keys"][0]["key_str"],
                 host=test_vars["host"],
                 port=test_vars["port"],
                 tls_port=test_vars["tls_port"],
-                tls=test_vars["encrypted"])
+                tls=test_vars["tls"]))
 
         reported_time = ably.time()
         actual_time = time.time() * 1000.0
@@ -26,17 +27,17 @@ class TestRestTime(unittest.TestCase):
                 msg="Time is not within 2 seconds")
 
     def test_time_without_key_or_token(self):
-        ably = AblyRest(host=test_vars["host"],
+        ably = AblyRest(Options(host=test_vars["host"],
                 port=test_vars["port"],
                 tls_port=test_vars["tls_port"],
-                tls=test_vars["encrypted"])
+                tls=test_vars["tls"]))
 
         ably.time()
     
     def test_time_fails_without_valid_host(self):
-        ably = AblyRest(host="this.host.does.not.exist",
+        ably = AblyRest(Options(host="this.host.does.not.exist",
                 port=test_vars["port"],
-                tls_port=test_vars["tls_port"])
+                tls_port=test_vars["tls_port"]))
 
         self.assertRaises(AblyException, ably.time)
 
