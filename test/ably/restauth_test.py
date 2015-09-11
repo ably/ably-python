18c7e1bd5fe94b26049d73adc92ae07de3bbee34
Helio Meira Lins <helio@vinta.com.br>
2015-09-11 18:46:30 -0300

    Test for RSA10e

--- test/ably/restauth_test.py
+++ test/ably/restauth_test.py
@@ -132,3 +132,15 @@ class TestAuthAuthorize(unittest.TestCase):
         token = self.ably.auth.authorise()
 
         self.assertIsInstance(token, TokenDetails)
+
+    def test_authorize_adhere_to_request_token(self):
+
+        token_params = {'ttl': 100}
+        auth_params = {'auth_url': 'http://somewhere.com'}
+
+        with mock.patch('ably.rest.auth.Auth.request_token') as request_mock:
+            self.ably.auth.authorise(auth_params=auth_params,
+                                     token_params=token_params)
+
+        request_mock.assert_called_once_with(auth_params=auth_params,
+                                             token_params=token_params)
