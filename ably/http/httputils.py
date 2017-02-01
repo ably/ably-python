a36e58c6e1f978596ee66306adcbbe0c3e9d1567
J. David Ibáñez <jdavid.ibp@gmail.com>
2017-02-01 19:35:25 +0100

    RSC7a and RSC7b: handle feedback (variants, etc.)

--- ably/http/httputils.py
+++ ably/http/httputils.py
@@ -14,10 +14,15 @@ class HttpUtils(object):
     }
 
     @staticmethod
-    def default_get_headers(binary=False):
+    def default_get_headers(binary=False, variant=None):
+        if variant is not None:
+            lib_version = 'python.%s-%s' % (variant, ably.lib_version)
+        else:
+            lib_version = 'python-%s' % ably.lib_version
+
         headers = {
             "X-Ably-Version": ably.api_version,
-            "X-Ably-Lib": 'python-%s' % ably.lib_version,
+            "X-Ably-Lib": 'python-%s' % lib_version,
         }
         if binary:
             headers["Accept"] = HttpUtils.mime_types['binary']
@@ -26,7 +31,7 @@ class HttpUtils(object):
         return headers
 
     @staticmethod
-    def default_post_headers(binary=False):
-        headers = HttpUtils.default_get_headers(binary=binary)
+    def default_post_headers(binary=False, variant=None):
+        headers = HttpUtils.default_get_headers(binary=binary, variant=variant)
         headers["Content-Type"] = headers["Accept"]
         return headers
