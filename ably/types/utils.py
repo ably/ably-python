0ac91dea7f000563e2660ff51282d46cb1704719
J. David Ibáñez <jdavid.ibp@gmail.com>
2018-08-02 11:22:04 +0200

    Fix tests
    Started failing with deviceIdentityToken error

--- ably/types/utils.py
+++ ably/types/utils.py
@@ -1,7 +1,12 @@
 import re
 
-def camel_to_snake(name, first_cap_re = re.compile('(.)([A-Z][a-z]+)')):
-    return first_cap_re.sub(r'\1_\2', name).lower()
+
+first_cap_re = re.compile('(.)([A-Z][a-z]+)')
+all_cap_re = re.compile('([a-z0-9])([A-Z])')
+def camel_to_snake(name):
+    s1 = first_cap_re.sub(r'\1_\2', name)
+    return all_cap_re.sub(r'\1_\2', s1).lower()
+
 
 def snake_to_camel(name):
     name = name.split('_')
