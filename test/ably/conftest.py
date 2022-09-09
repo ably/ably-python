37c406e4b2986368c54c319622e95d5c3f9230a9
Owen Pearson <owen.pearson@ably.com>
2022-09-09 19:12:16 +0100

    Add pytest-asyncio fixture to get rest client

--- test/ably/conftest.py
+++ test/ably/conftest.py
@@ -1,6 +1,7 @@
 import asyncio
 
 import pytest
+import pytest_asyncio
 from test.ably.restsetup import RestSetup
 
 
@@ -10,3 +11,12 @@ def event_loop():
     loop.run_until_complete(RestSetup.get_test_vars())
     yield loop
     loop.run_until_complete(RestSetup.clear_test_vars())
+
+
+@pytest_asyncio.fixture(params=["json", "msgpack"])
+async def rest(request):
+    protocol = request.param
+    use_binary_protocol = protocol == "msgpack"
+    ably = await RestSetup.get_ably_rest(use_binary_protocol=use_binary_protocol)
+    yield ably
+    await ably.close()
