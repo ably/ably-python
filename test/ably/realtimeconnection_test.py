92cc1aef6b83a0506612f49a4075bb52c5fbb3a5
Owen Pearson <owen.pearson@ably.com>
2022-09-13 17:14:34 +0100

    Add tests for transient connection states

--- test/ably/realtimeconnection_test.py
+++ test/ably/realtimeconnection_test.py
@@ -1,3 +1,4 @@
+import asyncio
 from ably.realtime.connection import ConnectionState
 import pytest
 from ably.util.exceptions import AblyAuthException
@@ -17,6 +18,22 @@ class TestRealtimeAuth(BaseAsyncTestCase):
         assert ably.connection.state == ConnectionState.CONNECTED
         await ably.close()
         assert ably.connection.state == ConnectionState.CLOSED
+
+    async def test_connecting_state(self):
+        ably = await RestSetup.get_ably_realtime()
+        task = asyncio.create_task(ably.connect())
+        await asyncio.sleep(0)
+        assert ably.connection.state == ConnectionState.CONNECTING
+        await task
+        await ably.close()
+
+    async def test_closing_state(self):
+        ably = await RestSetup.get_ably_realtime()
+        await ably.connect()
+        task = asyncio.create_task(ably.close())
+        await asyncio.sleep(0)
+        assert ably.connection.state == ConnectionState.CLOSING
+        await task
 
     async def test_auth_invalid_key(self):
         ably = await RestSetup.get_ably_realtime(key=self.valid_key_format)
