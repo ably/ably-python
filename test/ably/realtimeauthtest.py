3a3a3294ce3098713575c8e4ad15fc0b3e000035
Owen Pearson <owen.pearson@ably.com>
2022-09-13 16:27:33 +0100

    Update tests to use realtime helper method

--- test/ably/realtimeauthtest.py
+++ test/ably/realtimeauthtest.py
@@ -8,21 +8,21 @@ from test.ably.utils import BaseAsyncTestCase
 class TestRealtimeAuth(BaseAsyncTestCase):
     async def setUp(self):
         self.test_vars = await RestSetup.get_test_vars()
-        self.valid_key_format = "Vjdw.owt:R97sjjjwer"
+        self.valid_key_format = "api:key"
 
     async def test_auth_with_valid_key(self):
-        ably = AblyRealtime(self.test_vars["keys"][0]["key_str"])
+        ably = await RestSetup.get_ably_realtime(key=self.test_vars["keys"][0]["key_str"])
         assert Auth.Method.BASIC == ably.auth.auth_mechanism, "Unexpected Auth method mismatch"
         assert ably.auth.auth_options.key_name == self.test_vars["keys"][0]['key_name']
         assert ably.auth.auth_options.key_secret == self.test_vars["keys"][0]['key_secret']
 
     async def test_auth_incorrect_key(self):
         with pytest.raises(AblyAuthException):
-            AblyRealtime("some invalid key")
+            await RestSetup.get_ably_realtime(key="some invalid key")
 
     async def test_auth_with_valid_key_format(self):
         key = self.valid_key_format.split(":")
-        ably = AblyRealtime(self.valid_key_format)
+        ably = await RestSetup.get_ably_realtime(key=self.valid_key_format)
         assert Auth.Method.BASIC == ably.auth.auth_mechanism, "Unexpected Auth method mismatch"
         assert ably.auth.auth_options.key_name == key[0]
         assert ably.auth.auth_options.key_secret == key[1]
@@ -34,7 +34,6 @@ class TestRealtimeAuth(BaseAsyncTestCase):
     #     assert "connectionDetails" in conn
 
     async def test_auth_invalid_key(self):
-        ably = AblyRealtime(self.valid_key_format)
+        ably = await RestSetup.get_ably_realtime(key=self.valid_key_format)
         with pytest.raises(AblyAuthException):
             await ably.connection.connect()
-        
