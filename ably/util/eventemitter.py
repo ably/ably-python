e2aec263ddfd717d7e455405bae83f6fdcf2e0ef
Owen Pearson <owen.pearson@ably.com>
2022-11-01 12:32:12 +0000

    doc: add docstrings for patched EventEmitter

--- ably/util/eventemitter.py
+++ ably/util/eventemitter.py
@@ -18,11 +18,37 @@ def _is_all_event_args(*args):
 
 
 class EventEmitter:
+    """
+    A generic interface for event registration and delivery used in a number of the types in the Realtime client
+    library. For example, the Connection object emits events for connection state using the EventEmitter pattern.
+
+    Methods
+    -------
+    on(*args)
+        Attach to channel
+    once(*args)
+        Detach from channel
+    off()
+        Subscribe to messages on a channel
+    """
     def __init__(self):
         self.__named_event_emitter = AsyncIOEventEmitter()
         self.__all_event_emitter = AsyncIOEventEmitter()
 
     def on(self, *args):
+        """
+        Registers the provided listener for the specified event, if provided, and otherwise for all events.
+        If on() is called more than once with the same listener and event, the listener is added multiple times to
+        its listener registry. Therefore, as an example, assuming the same listener is registered twice using
+        on(), and an event is emitted once, the listener would be invoked twice.
+
+        Parameters
+        ----------
+        name : str
+            The named event to listen for.
+        listener : callable
+            The event listener.
+        """
         if _is_all_event_args(*args):
             self.__all_event_emitter.add_listener(_all_event, args[0])
         elif _is_named_event_args(*args):
@@ -31,6 +57,20 @@ class EventEmitter:
             raise ValueError("EventEmitter.on(): invalid args")
 
     def once(self, *args):
+        """
+        Registers the provided listener for the first event that is emitted. If once() is called more than once
+        with the same listener, the listener is added multiple times to its listener registry. Therefore, as an
+        example, assuming the same listener is registered twice using once(), and an event is emitted once, the
+        listener would be invoked twice. However, all subsequent events emitted would not invoke the listener as
+        once() ensures that each registration is only invoked once.
+
+        Parameters
+        ----------
+        name : str
+            The named event to listen for.
+        listener : callable
+            The event listener.
+        """
         if _is_all_event_args(*args):
             self.__all_event_emitter.once(_all_event, args[0])
         elif _is_named_event_args(*args):
@@ -39,6 +79,17 @@ class EventEmitter:
             raise ValueError("EventEmitter.once(): invalid args")
 
     def off(self, *args):
+        """
+        Removes all registrations that match both the specified listener and, if provided, the specified event.
+        If called with no arguments, deregisters all registrations, for all events and listeners.
+
+        Parameters
+        ----------
+        name : str
+            The named event to listen for.
+        listener : callable
+            The event listener.
+        """
         if len(args) == 0:
             self.__all_event_emitter.remove_all_listeners()
             self.__named_event_emitter.remove_all_listeners()
