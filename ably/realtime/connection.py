e25992bf48c47bcfe2e911708c6aef30435ce633
Owen Pearson <owen.pearson@ably.com>
2023-01-16 22:40:01 +0000

    refactor(ConnectionDetails): add max_idle_interval property

--- ably/realtime/connection.py
+++ ably/realtime/connection.py
@@ -49,13 +49,15 @@ class ConnectionStateChange:
 @dataclass
 class ConnectionDetails:
     connection_state_ttl: int
+    max_idle_interval: int
 
-    def __init__(self, connection_state_ttl: int):
+    def __init__(self, connection_state_ttl: int, max_idle_interval: int):
         self.connection_state_ttl = connection_state_ttl
+        self.max_idle_interval = max_idle_interval
 
     @staticmethod
     def from_dict(json_dict: dict):
-        return ConnectionDetails(json_dict.get('connectionStateTtl'))
+        return ConnectionDetails(json_dict.get('connectionStateTtl'), json_dict.get('maxIdleInterval'))
 
 
 class Connection(EventEmitter):
