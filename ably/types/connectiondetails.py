e5fb0ffa95ed0de69c2c4c51b384f5311a282bc0
moyosore <mohyour01@gmail.com>
2023-01-30 15:28:50 +0000

    update connection details with connection key and id

--- ably/types/connectiondetails.py
+++ ably/types/connectiondetails.py
@@ -5,11 +5,17 @@ from dataclasses import dataclass
 class ConnectionDetails:
     connection_state_ttl: int
     max_idle_interval: int
+    connection_key: str
+    connection_id: str
 
-    def __init__(self, connection_state_ttl: int, max_idle_interval: int):
+    def __init__(self, connection_state_ttl: int, max_idle_interval: int,
+                 connection_key: str, connection_id: str):
         self.connection_state_ttl = connection_state_ttl
         self.max_idle_interval = max_idle_interval
+        self.connection_key = connection_key
+        self.connection_id = connection_id
 
     @staticmethod
     def from_dict(json_dict: dict):
-        return ConnectionDetails(json_dict.get('connectionStateTtl'), json_dict.get('maxIdleInterval'))
+        return ConnectionDetails(json_dict.get('connectionStateTtl'), json_dict.get('maxIdleInterval'),
+                                 json_dict.get('connectionKey'), json_dict.get('connectionId'))
