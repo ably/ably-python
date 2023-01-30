9d4cfc15e14b7a96b325e7216569d1bb123b4c91
Owen Pearson <owen.pearson@ably.com>
2023-01-30 17:15:31 +0000

    test: add tests for invalid resume response

--- test/ably/realtimeresume_test.py
+++ test/ably/realtimeresume_test.py
@@ -1,6 +1,7 @@
 from ably.realtime.connection import ConnectionState
+from ably.realtime.realtime_channel import ChannelState
 from test.ably.restsetup import RestSetup
-from test.ably.utils import BaseAsyncTestCase
+from test.ably.utils import BaseAsyncTestCase, random_string
 
 
 class TestRealtimeResume(BaseAsyncTestCase):
@@ -36,4 +37,71 @@ class TestRealtimeResume(BaseAsyncTestCase):
         state_change = await ably.connection.once_async(ConnectionState.FAILED)
         assert state_change.reason.code == 40101
         assert state_change.reason.status_code == 401
+        await ably.close()
+
+    async def test_invalid_resume_response(self):
+        ably = await RestSetup.get_ably_realtime()
+
+        await ably.connection.once_async(ConnectionState.CONNECTED)
+
+        assert ably.connection.connection_manager.connection_details
+        ably.connection.connection_manager.connection_details.connection_key = 'ably-python-fake-key'
+
+        assert ably.connection.connection_manager.transport
+        await ably.connection.connection_manager.transport.dispose()
+        ably.connection.connection_manager.notify_state(ConnectionState.DISCONNECTED)
+
+        state_change = await ably.connection.once_async(ConnectionState.CONNECTED)
+
+        assert state_change.reason.code == 80018
+        assert state_change.reason.status_code == 400
+        assert ably.connection.error_reason == state_change.reason
+
+        await ably.close()
+
+    async def test_attached_channel_reattaches_on_invalid_resume(self):
+        ably = await RestSetup.get_ably_realtime()
+
+        await ably.connection.once_async(ConnectionState.CONNECTED)
+
+        channel = ably.channels.get(random_string(5))
+
+        await channel.attach()
+
+        assert ably.connection.connection_manager.connection_details
+        ably.connection.connection_manager.connection_details.connection_key = 'ably-python-fake-key'
+
+        assert ably.connection.connection_manager.transport
+        await ably.connection.connection_manager.transport.dispose()
+        ably.connection.connection_manager.notify_state(ConnectionState.DISCONNECTED)
+
+        await ably.connection.once_async(ConnectionState.CONNECTED)
+
+        assert channel.state == ChannelState.ATTACHING
+
+        await channel.once_async(ChannelState.ATTACHED)
+
+        await ably.close()
+
+    async def test_suspended_channel_reattaches_on_invalid_resume(self):
+        ably = await RestSetup.get_ably_realtime()
+
+        await ably.connection.once_async(ConnectionState.CONNECTED)
+
+        channel = ably.channels.get(random_string(5))
+        channel.state = ChannelState.SUSPENDED
+
+        assert ably.connection.connection_manager.connection_details
+        ably.connection.connection_manager.connection_details.connection_key = 'ably-python-fake-key'
+
+        assert ably.connection.connection_manager.transport
+        await ably.connection.connection_manager.transport.dispose()
+        ably.connection.connection_manager.notify_state(ConnectionState.DISCONNECTED)
+
+        await ably.connection.once_async(ConnectionState.CONNECTED)
+
+        assert channel.state == ChannelState.ATTACHING
+
+        await channel.once_async(ChannelState.ATTACHED)
+
         await ably.close()
