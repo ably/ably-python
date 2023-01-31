13a2deca6dea6526bf0dd8e7ad3cb07b02b85b22
Owen Pearson <owen.pearson@ably.com>
2023-01-31 16:12:49 +0000

    refactor: add Flags enum

--- ably/realtime/realtime_channel.py
+++ ably/realtime/realtime_channel.py
@@ -25,6 +25,20 @@ class ChannelState(str, Enum):
     FAILED = 'failed'
 
 
+class Flags(int, Enum):
+    # Channel attach state flags
+    HAS_PRESENCE = 1 << 0
+    HAS_BACKLOG = 1 << 1
+    RESUMED = 1 << 2
+    TRANSIENT = 1 << 4
+    ATTACH_RESUME = 1 << 5
+    # Channel mode flags
+    PRESENCE = 1 << 16
+    PUBLISH = 1 << 17
+    SUBSCRIBE = 1 << 18
+    PRESENCE_SUBSCRIBE = 1 << 19
+
+
 @dataclass
 class ChannelStateChange:
     previous: ChannelState
