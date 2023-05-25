2d65f1c44744161ac15ee55798e0ad244bf018e9
Owen Pearson <owen.pearson@ably.com>
2023-05-25 12:04:59 +0100

    feat!: use api v3 and untyped stats

--- test/ably/rest/resthttp_test.py
+++ test/ably/rest/resthttp_test.py
@@ -187,7 +187,7 @@ class TestRestHttp(BaseAsyncTestCase):
 
         # API
         assert 'X-Ably-Version' in r.request.headers
-        assert r.request.headers['X-Ably-Version'] == '2'
+        assert r.request.headers['X-Ably-Version'] == '3'
 
         # Agent
         assert 'Ably-Agent' in r.request.headers
