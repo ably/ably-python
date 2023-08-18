22dbfb54f2bac57d16326b5ce58f63dd1dcef772
sacOO7 <sachinshinde7676@gmail.com>
2023-08-18 16:11:00 +0530

    Added unit tests for inner message fields

---
+++ test/unit/message_test.py
@@ -0,0 +1,50 @@
+import ably.types.message
+
+
+# TM2a, TM2c, TM2f
+def test_update_inner_message_fields_tm2():
+    proto_msg: dict = {
+        'id': 'abcdefg',
+        'connectionid': 'custom_connection_id',
+        'timestamp': 23134,
+        'messages': [
+            {
+                'event': 'test',
+                'data': 'hello there'
+            }
+        ]
+    }
+    ably.types.message.Message.update_inner_message_fields(proto_msg)
+    messages: list[dict] = proto_msg.get('messages')
+    msg_index = 0
+    for msg in messages:
+        assert msg.get('id') == f"abcdefg:{msg_index}"
+        assert msg.get('connectionid') == 'custom_connection_id'
+        assert msg.get('timestamp') == 23134
+        msg_index = msg_index + 1
+
+
+# TM2a, TM2c, TM2f
+def test_update_inner_message_fields_for_presence_msg_tm2():
+    proto_msg: dict = {
+        'id': 'abcdefg',
+        'connectionid': 'custom_connection_id',
+        'timestamp': 23134,
+        'presence': [
+            {
+                'message': {
+                    'event': 'test',
+                    'data': 'hello there'
+                },
+            }
+        ]
+    }
+    ably.types.message.Message.update_inner_message_fields(proto_msg)
+    presence_messages: list[dict] = proto_msg.get('presence')
+    msg_index = 0
+    for presence_msg in presence_messages:
+        msg = presence_msg.get('message')
+        assert msg.get('id') == f"abcdefg:{msg_index}"
+        assert msg.get('connectionid') == 'custom_connection_id'
+        assert msg.get('timestamp') == 23134
+        msg_index = msg_index + 1
