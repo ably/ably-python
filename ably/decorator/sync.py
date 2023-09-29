51f1d819566e35970d048aec22ebe36bf7d3979e
sacOO7 <sachinshinde7676@gmail.com>
2023-09-30 02:10:36 +0530

    simplified sync decorator

--- ably/decorator/sync.py
+++ ably/decorator/sync.py
@@ -21,17 +21,16 @@ def optional_sync(fn):
             pass
         ably_eventloop: events = AblyEventLoop.current().loop
 
-        res = fn(*args, **kwargs)
-        if asyncio.iscoroutine(res):
-            # Handle calls from ably_eventloop on the same loop, return awaitable
-            if caller_eventloop is not None and caller_eventloop == ably_eventloop:
-                return ably_eventloop.create_task(res)
+        # Handle calls from ably_eventloop on the same loop, return awaitable
+        if caller_eventloop is not None and caller_eventloop == ably_eventloop:
+            return ably_eventloop.create_task(fn(*args, **kwargs))
 
-            # Post external calls on ably_eventloop, return awaitable on calling eventloop
-            future = asyncio.run_coroutine_threadsafe(res, ably_eventloop)
-            if caller_eventloop is not None and caller_eventloop.is_running():
-                return asyncio.wrap_future(future)
-            return future.result()
-        return res
+        # Handle calls from external eventloop, post them on ably_eventloop
+        future = asyncio.run_coroutine_threadsafe(fn(*args, **kwargs), ably_eventloop)
+        if caller_eventloop is not None and caller_eventloop.is_running():
+            return asyncio.wrap_future(future)
+
+        # If called from regular function, return blocking result
+        return future.result()
 
     return wrapper
