f4608cd3382f4f6517f932619841327e1112d627
sacOO7 <sachinshinde7676@gmail.com>
2023-09-30 03:45:55 +0530

    Updated eventloop to stop instead of close

--- ably/executer/eventloop.py
+++ ably/executer/eventloop.py
@@ -4,8 +4,8 @@ from asyncio import events
 
 
 class AppEventLoop:
-    loop: events = None
-    thread: threading = None
+    loop: events
+    thread: threading
     active: 'AppEventLoop' = None
 
     def __init__(self):
@@ -15,7 +15,7 @@ class AppEventLoop:
     @staticmethod
     def current() -> 'AppEventLoop':
         if (AppEventLoop.active is None or
-                AppEventLoop.active.loop.is_closed()):
+                not AppEventLoop.active.loop.is_running()):
             AppEventLoop.active = AppEventLoop()
             AppEventLoop.active.__create_if_not_exist()
         return AppEventLoop.active
@@ -30,7 +30,6 @@ class AppEventLoop:
             self.thread.start()
 
     def close(self) -> events:
-        if self.loop is not None and not self.loop.is_closed:
+        if self.loop is not None and self.loop.is_running():
             self.loop.call_soon_threadsafe(self.loop.stop)
             self.thread.join()
-            self.loop.close()
