97fc1043bd89491cc0fa03f0d7953ddc44581fb6
sacOO7 <sachinshinde7676@gmail.com>
2023-10-02 15:44:30 +0530

    refactored app event loop

--- ably/executer/eventloop.py
+++ ably/executer/eventloop.py
@@ -6,30 +6,32 @@ from asyncio import events
 class AppEventLoop:
     loop: events
     thread: threading
-    active: 'AppEventLoop' = None
+    _global: 'AppEventLoop' = None
+    is_active: bool
 
     def __init__(self):
         self.loop = None
         self.thread = None
+        self.is_active = False
 
     @staticmethod
     def current() -> 'AppEventLoop':
-        if (AppEventLoop.active is None or
-                not AppEventLoop.active.loop.is_running()):
-            AppEventLoop.active = AppEventLoop()
-            AppEventLoop.active.__create_if_not_exist()
-        return AppEventLoop.active
+        if AppEventLoop._global is None or not AppEventLoop._global.is_active:
+            AppEventLoop._global = AppEventLoop()
+            AppEventLoop._global.create_safe()
+        return AppEventLoop._global
 
-    def __create_if_not_exist(self):
-        if self.loop is None or self.loop.is_closed():
-            self.loop = asyncio.new_event_loop()
-        if not self.loop.is_running():
-            self.thread = threading.Thread(
-                target=self.loop.run_forever,
-                daemon=True)
-            self.thread.start()
+    def create_safe(self):
+        if self.is_active:
+            self.close()
+        self.loop = asyncio.new_event_loop()
+        self.thread = threading.Thread(target=self.loop.run_forever)
+        self.thread.start()
+        self.is_active = True
 
     def close(self) -> events:
         if self.loop is not None and self.loop.is_running():
             self.loop.call_soon_threadsafe(self.loop.stop)
+        if self.thread is not None:
             self.thread.join()
+        self.is_active = False
