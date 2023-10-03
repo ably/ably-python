2a7c4fe869a0b45915924fd9e1b612380d22c1d2
sacOO7 <sachinshinde7676@gmail.com>
2023-10-03 12:50:15 +0530

    Refactored app event loop

--- ably/executer/eventloop.py
+++ ably/executer/eventloop.py
@@ -18,17 +18,20 @@ class AppEventLoop:
     @staticmethod
     def get_global() -> 'AppEventLoop':
         if AppEventLoop._global is None or not AppEventLoop._global.is_active:
-            AppEventLoop._global = AppEventLoop.create()
+            AppEventLoop._global = AppEventLoop.create(True)
         return AppEventLoop._global
 
     @staticmethod
-    def create() -> 'AppEventLoop':
+    def create(background=False) -> 'AppEventLoop':
         app_loop = AppEventLoop()
         app_loop.loop = asyncio.new_event_loop()
-        app_loop.thread = threading.Thread(target=app_loop.loop.run_forever)
+        app_loop.thread = threading.Thread(target=app_loop.loop.run_forever, daemon=background)
         app_loop.thread.start()
         app_loop.is_active = True
         return app_loop
+
+    def run(self, coro, callback):
+        self.loop.call_soon()
 
     def run_sync(self, coro):
         # todo - can only handle run_sync from different thread than app_loop
@@ -45,9 +48,18 @@ class AppEventLoop:
         future = asyncio.run_coroutine_threadsafe(coro, self.loop)
         return future.result()
 
-    def close(self) -> events:
+    def run_async(self, coro):
+        future = asyncio.run_coroutine_threadsafe(coro, self.loop)
+        return asyncio.wrap_future(future)
+
+    def _close(self):
         if self.loop is not None and self.loop.is_running():
             self.loop.call_soon_threadsafe(self.loop.stop)
         if self.thread is not None:
             self.thread.join()
         self.is_active = False
+
+    def close(self) -> events:
+        if self == AppEventLoop._global:  # Global eventloop is shared amongst multiple client instances.
+            return                        # It will be reused, and closed automatically once process ends.
+        self._close()
